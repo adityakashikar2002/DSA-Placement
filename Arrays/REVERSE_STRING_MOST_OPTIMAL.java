da3b266e53ee00fa395ae7f79793d2a08e83b003
Aditya Kashikar <150321037+adityakashikar2002@users.noreply.github.com>
2024-08-11 15:35:47 +0530

    Update REVERSE_STRING_MOST_OPTIMAL.java

--- Arrays/REVERSE_STRING_MOST_OPTIMAL.java
+++ Arrays/REVERSE_STRING_MOST_OPTIMAL.java
@@ -15,3 +15,34 @@ class Solution {
     return rse;
   }
 }
+
+//----------------------------------------
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "hello";
+        String reversed = reverseString(input);
+        System.out.println(reversed);  // Output: "olleh"
+    }
+
+    public static String reverseString(String s) {
+        // Convert string to a character array because strings are immutable in Java
+        char[] charArray = s.toCharArray();
+        
+        int left = 0;
+        int right = charArray.length - 1;
+        
+        while (left < right) {
+            // Swap characters
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            
+            left++;
+            right--;
+        }
+        
+        // Convert character array back to string
+        return new String(charArray);
+    }
+}
+
