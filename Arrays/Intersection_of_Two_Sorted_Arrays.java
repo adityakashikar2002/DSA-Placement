ff385a83a436340741455c58afd68a5fe0923490
Aditya Kashikar <150321037+adityakashikar2002@users.noreply.github.com>
2024-08-16 19:45:52 +0530

    Create Intersection_of_Two_Sorted_Arrays.java

---
+++ Arrays/Intersection_of_Two_Sorted_Arrays.java
@@ -0,0 +1,26 @@
+import java.util.* ;
+import java.io.*; 
+public class Intersection_of_Two_Sorted_Arrays
+{
+	public static ArrayList<Integer> findArrayIntersection(ArrayList<Integer> arr1, int n, ArrayList<Integer> arr2, int m)
+	{
+		// Write Your Code Here.
+		int i=0,j=0;
+		ArrayList<Integer> list = new ArrayList<>();
+
+		while(i<n&&j<m)
+		{
+			if(arr1.get(i)<arr2.get(j))
+				i++;
+			else if(arr1.get(i)>arr2.get(j))
+				j++;
+			else if(arr1.get(i).equals(arr2.get(j)))
+			{
+				list.add(arr1.get(i));
+				i++;
+				j++;
+			}
+		}
+		return list;
+	}
+}
