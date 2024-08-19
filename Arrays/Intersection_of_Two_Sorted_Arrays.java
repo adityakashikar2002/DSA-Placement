2164592aa594c236b732bf9795b220c3830b78ca
Aditya Kashikar <150321037+adityakashikar2002@users.noreply.github.com>
2024-08-19 21:36:52 +0530

    Update Intersection_of_Two_Sorted_Arrays.java

--- Arrays/Intersection_of_Two_Sorted_Arrays.java
+++ Arrays/Intersection_of_Two_Sorted_Arrays.java
@@ -24,3 +24,29 @@ public class Intersection_of_Two_Sorted_Arrays
 		return list;
 	}
 }
+//---------------------------------------------------------------------------------------------------------------------------------------------
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) 
+    {
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+
+        for(int i:nums1)
+        {
+            s1.add(i);
+        }
+
+        for(int i:nums2)
+        {
+            if(s1.contains(i))
+                s2.add(i);
+        }
+
+        int[] res = new int[s2.size()];
+        int k=0;
+        for(int i:s2)
+            res[k++]=i;
+
+        return res;    
+    }
+}
