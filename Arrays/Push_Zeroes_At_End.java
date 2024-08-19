8f805977a56c45c6bce488b9f4d66e7e14f4a282
Aditya Kashikar <150321037+adityakashikar2002@users.noreply.github.com>
2024-08-19 21:46:43 +0530

    Update Push_Zeroes_At_End.java

--- Arrays/Push_Zeroes_At_End.java
+++ Arrays/Push_Zeroes_At_End.java
@@ -22,3 +22,22 @@ public class PushZeroesAtEnd
         
 	}
 }
+//-----------------------------------------------------------------------------------
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+		int nonZeroIndex=0;
+
+		for(int i=0;i<n;i++)
+		{
+			if(nums[i]!=0)
+			{
+				nums[nonZeroIndex]=nums[i];
+				nonZeroIndex++;
+			}
+		}
+
+		for(int i=nonZeroIndex;i<n;i++)
+			nums[i]=0;
+    }
+}
