f046405fe4d7a1d511e830448083a76524602c59
Aditya Kashikar <150321037+adityakashikar2002@users.noreply.github.com>
2024-11-15 13:54:23 +0530

    Update (V.IMP)_Queue_using_Two_Stacks.java

--- Queues/(V.IMP)_Queue_using_Two_Stacks.java
+++ Queues/(V.IMP)_Queue_using_Two_Stacks.java
@@ -1,3 +1,39 @@
+class StackQueue
+{
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    //Function to push an element in queue by using 2 stacks.
+    void Push(int x)
+    {
+	   // Your code here
+	    s1.push(x);
+	    
+    }
+	
+    
+    //Function to pop an element from queue by using 2 stacks.
+    int Pop()
+    {
+	   // Your code here
+	    
+	   if(s1.empty()&&s2.empty())
+	   {
+	       return -1;
+	   }
+	   else if(s2.isEmpty())
+	   {
+	       while(!s1.isEmpty())
+	       {
+	            s2.push(s1.pop());
+	       }
+	   }
+	   return s2.pop();
+    }
+}
+
+//-----------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
 class Queue
 {
     Stack<Integer> input = new Stack<Integer>(); 
